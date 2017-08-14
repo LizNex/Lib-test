4bb9414a6ccf0899db4196bacad42cf7ef43faf8
左手喝水 <360347556@qq.com>
2017-08-14 15:08:19 +0800

    添加panelsilder，更新common

--- plugin/common/common.js
+++ plugin/common/common.js
@@ -1,4 +1,23 @@
-﻿// 重写alert和confirm方法，让苹果手机不出现地址
+﻿//--------依赖jquery----------
+
+//--------工具类---------------
+//获得对象是页面中相同类名的第几个对象
+//obj（obj）：对象  className（str）：类名
+function getClassIndex(obj, className) {
+    var group = document.getElementsByClassName(className);
+    var arr = Array.prototype.slice.call(group, 0);
+    return arr.indexOf(obj)
+}
+
+
+
+// 类数组转化为数组
+// likeArray(类数组)
+function likeToArray(likeArray) {
+    return Array.prototype.slice.call(likeArray, 0);
+}
+
+// 重写alert方法，让苹果手机不出现地址
 window.alert = function (name) {
     var iframe = document.createElement("IFRAME");
     iframe.style.display = "none";
@@ -8,7 +27,7 @@ window.alert = function (name) {
     iframe.parentNode.removeChild(iframe);
 };
 
-
+// 重写confirm方法，让苹果手机不出现地址
 window.confirm = function (message) {
     var iframe = document.createElement("IFRAME");
     iframe.style.display = "none";
@@ -20,16 +39,22 @@ window.confirm = function (message) {
     return result;
 };
 
+// 显示loadingF
+function showLoad() {
+    $('.loading').css('display', 'block');
+}
+// 关闭loading
+function closeLoad() {
 
-//获得对象是页面中相同类名的第几个对象
-//obj（obj）：对象  className（str）：类名
-function getClassIndex(obj, className) {
-    var group = document.getElementsByClassName(className);
-    var arr = Array.prototype.slice.call(group, 0);
-    return arr.indexOf(obj)
+    $('.loading').css('display', 'none');
+
 }
 
-//格式化日期  str（str）:日期  format(str):格式 例'yyyy/mm/dd'
+
+
+//--------业务类---------------
+//格式化日期  
+// str（str）:日期  format(str):格式 例'yyyy/mm/dd'
 function formatDate(str, format) {
     var date = new Date(str.replace(/(-0|-)/g, '/'))
     var obj = {
@@ -41,9 +66,6 @@ function formatDate(str, format) {
         return obj[$1]
     });
 }
-
-
-
 
 //格式化时间将ele的时间starttime endtime转化为startDate time,转化的结果封装成对象返回
 //ele(obj):带有starttime endtime的对象
@@ -74,10 +96,14 @@ function skip(url) {
     window.location.href = url;
 }
 
+//返回上一个页面并刷新
+function skipBack() {
+    window.history.back(-1) && location.reload();
+}
 
 //获取点击对象的data-*属性，并将属性放到url后面作为下个页面的参数,跳转到此url
 //event(obj):点击事件 url(str):地址 
-function skipParameter(event, url) {
+function skipData(event, url) {
     var temp = event.currentTarget;
     var url = url + "?";
     var pop = temp.dataset;
@@ -104,3 +130,31 @@ function numOnly(input) {
     }).bind("paste", function () {}).css("ime-mode", "disabled");
 }
 
+
+//点击切换class,html上要加data-toggle属性
+// event(obj):点击事件,classOn(str):on状态的class
+function clickStyle(event, classOn) {
+    var obj = event.currentTarget;
+    var toggle = obj.dataset.toggle === "true";
+    if (toggle) {
+        $(obj).addClass(classOn);
+    } else {
+        $(obj).removeClass(classOn);
+    }
+    obj.dataset.toggle = !toggle;
+}
+
+//点击tab切换,通过添加、移除class改变样式
+//event(obj):点击事件,classTab(str):tab元素的类名,classOn(str):on状态的class
+function tabStyle(event, ClassTab, classOn) {
+    var tab = document.getElementsByClassName(ClassTab);
+    var tab = Array.prototype.slice.call(tab, 0);
+
+    function changStyle() {
+        var obj = event.currentTarget;
+        var index = tab.indexOf(obj);
+        $(obj).addClass(classOn)
+        $(tab[Math.abs(1 - index)]).removeClass(classOn)
+    }
+    return changStyle;
+}
